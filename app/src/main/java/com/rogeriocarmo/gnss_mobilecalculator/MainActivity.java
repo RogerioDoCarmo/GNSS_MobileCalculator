e71a195181067a86ce6755a89f257c37b421da29
Rogério do Carmo <rogerio.carmo02@gmail.com>
2018-05-11 09:35:44 -0300

    Commit Inicial!

---
+++ app/src/main/java/com/rogeriocarmo/gnss_mobilecalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rogeriocarmo.gnss_mobilecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
