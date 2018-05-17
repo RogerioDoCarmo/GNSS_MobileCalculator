0822d663c12c1f7d8fd361a10c3142d8e492d359
Rogério do Carmo <rogerio.carmo02@gmail.com>
2018-05-17 08:19:18 -0300

    Inicio Classe ReaderRINEX. Definição de getters e setters

--- app/src/main/java/Codigos/ReaderRINEX.java
+++ app/src/main/java/Codigos/ReaderRINEX.java
@@ -1,4 +1,13 @@
 package Codigos;
 
+import java.util.ArrayList;
+
 public class ReaderRINEX {
+
+    public ArrayList<GNSSNavMsg> listaNavMsgs;
+
+    public ReaderRINEX(String fileName){ //TODO Por enquanto pegar da pasta raw assets msm!
+        
+    }
+
 }
