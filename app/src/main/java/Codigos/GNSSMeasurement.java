9af5f2c9b0be7a41dee73d5e45439210a7ce86ff
Rogério do Carmo <rogerio.carmo02@gmail.com>
2018-05-17 07:12:56 -0300

    Commit Inicial!

---
+++ app/src/main/java/Codigos/GNSSMeasurement.java
@@ -0,0 +1,4 @@
+package Codigos;
+
+public class GNSSMeasurement {
+}
---
+++ app/src/main/java/Codigos/WLSpvtCalculator.java
@@ -0,0 +1,9 @@
+package Codigos;
+
+import java.util.ArrayList;
+
+public class WLSpvtCalculator {
+    public static ArrayList<String> lista;
+
+
+}
