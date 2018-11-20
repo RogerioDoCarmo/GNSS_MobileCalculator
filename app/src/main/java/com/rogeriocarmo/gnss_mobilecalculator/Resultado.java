8f5c0a225da932b0fb50020923e73efbea0c012c
Rogério do Carmo <rogerio.carmo02@gmail.com>
2018-11-20 11:12:54 -0200

    Atualização da Maps Activity

--- app/src/main/java/com/rogeriocarmo/gnss_mobilecalculator/Resultado.java
+++ app/src/main/java/com/rogeriocarmo/gnss_mobilecalculator/Resultado.java
@@ -1,6 +1,7 @@
 package com.rogeriocarmo.gnss_mobilecalculator;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -28,17 +29,20 @@ public class Resultado extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
+    private final double EP02_LAT = -22.122500;
+    private final double EP02_LONG = -51.407778;
+    LatLng coordEP02 = new LatLng(EP02_LAT, EP02_LONG);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -53,8 +57,8 @@ public class Resultado extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        googleMap.setMinZoomPreference(13.0f);
-        googleMap.setMaxZoomPreference(18.0f);
+        googleMap.setMinZoomPreference(14.0f);
+        googleMap.setMaxZoomPreference(24.0f);
 
 //        Ecef2LlaConverter.GeodeticLlaValues valores = (Ecef2LlaConverter.GeodeticLlaValues)
 //                getIntent().getSerializableExtra("Coord");
@@ -74,16 +78,25 @@ public class Resultado extends FragmentActivity implements OnMapReadyCallback {
         Double longFinal = resultados.get(resultados.size() - 1).getY();
         Double altFinal = resultados.get(resultados.size() - 1).getZ();
 
-        LatLng coord = new LatLng(latFinal, longFinal);
+        LatLng coordSolu = new LatLng(latFinal, longFinal);
 
-        mMap.addMarker(new MarkerOptions().position(coord).title("Solução Final"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coord,16));
+        mMap.addMarker(new MarkerOptions().
+                position(coordEP02).
+                title("Marco EP02").
+                snippet("Coordenadas do IBGE"));
+        mMap.addMarker(new MarkerOptions().
+                alpha(0.8f).
+                position(coordSolu).
+                title("Solução Final").
+                snippet("Coordenadas Calculadas"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordSolu,19));
 
         Snackbar snackbar = Snackbar
                 .make(Objects.requireNonNull(mapFragment.getActivity()).findViewById(R.id.map),
                          "Lat:  " +  new DecimalFormat("#.#### ").format(latFinal) +
-                             "Lon: " + new DecimalFormat("#.#### ").format(longFinal)  +
-                             "Alt: " + new DecimalFormat("#.### ").format(altFinal),
+                              "Lon: " + new DecimalFormat("#.#### ").format(longFinal)  +
+                              "Alt: " + new DecimalFormat("#.### ").format(altFinal),
                         Snackbar.LENGTH_INDEFINITE)
                 .setAction("Voltar", new View.OnClickListener() {
                     @Override
@@ -93,5 +106,11 @@ public class Resultado extends FragmentActivity implements OnMapReadyCallback {
                     }
                 });
         snackbar.show();
+
+        float[] result = new float[1];
+        Location.distanceBetween(coordSolu.latitude,coordSolu.longitude,
+                                 coordEP02.latitude,coordEP02.longitude, result);
+
+        Toast.makeText(this,"Distância em metros: " + result[0], Toast.LENGTH_LONG).show();
     }
 }
